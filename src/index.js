7755a50d5d67cc4fd42afbc0aab5c4b29f196d47
Finsoy <vadimka.cry.00@gmail.com>
2021-02-22 20:41:59 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let abs = Math.abs(n);
+    let num = abs.toString().split("").reverse().join("");
+    return +num;
+};
